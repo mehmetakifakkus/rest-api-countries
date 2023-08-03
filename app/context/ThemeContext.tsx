af581a49ab7ba4397aa3b4e5de7b47f56053d7ac
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-03 12:29:38 +0300

    Add theme context for the dark mode

---
+++ app/context/ThemeContext.tsx
@@ -0,0 +1,44 @@
+// create a context for topics
+"use client";
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+
+export type TopicContextType = {
+  darkMode: boolean;
+  setDarkMode: Dispatch<SetStateAction<boolean>>;
+};
+
+const ThemeContext = createContext<TopicContextType>({
+  darkMode: false,
+  setDarkMode: () => {},
+});
+
+// create a provider for components to consume and subscribe to changes
+export const ThemeProvider = ({ children }: any) => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const valueObj = { darkMode, setDarkMode };
+
+  if (
+    darkMode ||
+    (!("darkMode" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  // // Whenever the user explicitly chooses light mode
+  // localStorage.darkMode = JSON.stringify(darkMode);
+
+  return (
+    <ThemeContext.Provider value={valueObj}>{children}</ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext = () => useContext(ThemeContext);
