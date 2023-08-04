af5acd102b490ef2db3fc18d00e0fdff5b0739b6
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-04 23:50:18 +0300

    Add page details

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,7 +1,8 @@
-import { Country } from "@/types";
+import { Country, CurrenciesProps, NativeNameProps } from "@/types";
 import Image from "next/image";
-import Link from "next/link";
 import { getAllCountries } from "../lib/countries";
+import BackButton from "../components/BackButton";
+import CountryBorder from "../components/CountryBorder";
 
 type Props = { params: { id: string } };
 
@@ -14,75 +15,83 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: Props) {
   const { id } = params;
-  const res = await fetch(`https://restcountries.com/v3.1/name/${id}`);
+  const res = await fetch(
+    `https://restcountries.com/v3.1/${
+      id.length === 3 ? `alpha/${id}` : `name/${id}`
+    }`
+  );
+
   const country = (await res.json())[0];
   console.log(country);
 
+  let currency = country?.currencies
+    ? (Object.values(country?.currencies)[0] as CurrenciesProps)
+    : undefined;
+
+  let nativeName = country?.name.nativeName
+    ? (Object.values(country?.name.nativeName)[0] as NativeNameProps)
+    : undefined;
+
   return (
-    <main className="pt-32 bg-slate-100 dark:bg-slate-500 px-20 h-[100vh] dark:text-white">
-      <Link href="/" className="mb-4">
-        <button className="w-32 border border-slate-300 rounded mb-8 p-1 px-2 text-lg hover:bg-slate-600 hover:text-white shadow-md">
-          ← Back
-        </button>
-      </Link>
+    <main className="pt-32 bg-slate-100 dark:bg-slate-500 px-6 sm:px-20 h-[100vh] dark:text-white">
+      <BackButton />
       <div className="flex flex-col md:flex-row gap-8 justify-between">
         <div className="max-w-[480px]">
           <Image
             className="aspect-video border shadow-lg"
-            src={country?.flags.png!}
-            alt={country?.name.common!}
+            src={country?.flags.png}
+            alt={country?.flags.alt || country?.name.common}
             width={600}
             height={420}
           />
         </div>
-        <div className="flex flex-col gap-4">
-          <h1 className="text-4xl font-bold">{country?.name.common}</h1>
+        <div className="flex flex-col gap-4 sm:min-w-[400px] md:min-w-[500px]">
+          <h1 className="text-4xl font-semibold">{country?.name.common}</h1>
           <div className="grid grid-cols-2 gap-8">
             <div className="flex flex-col gap-2">
+              {nativeName && (
+                <p>
+                  <span className="font-semibold">Native Name: </span>
+                  {nativeName.common}
+                </p>
+              )}
               <p>
-                <span className="font-bold">Native Name: </span>
-                {country?.name.common}
+                <span className="font-semibold">Population: </span>
+                {country?.population.toLocaleString()}
               </p>
               <p>
-                <span className="font-bold">Population: </span>
-                {country?.population}
-              </p>
-              <p>
-                <span className="font-bold">Region: </span>
+                <span className="font-semibold">Region: </span>
                 {country?.region}
               </p>
               <p>
-                <span className="font-bold">Sub Region: </span>
+                <span className="font-semibold">Sub Region: </span>
                 {country?.subregion}
               </p>
               <p>
-                <span className="font-bold">Capital: </span>
+                <span className="font-semibold">Capital: </span>
                 {country?.capital}
               </p>
             </div>
             <div className="flex flex-col gap-2">
               <p>
-                <span className="font-bold">Native Name: </span>
-                {country?.name.common}
+                <span className="font-semibold">Top level domain: </span>
+                {country?.tld[0]}
               </p>
-              <p>
-                <span className="font-bold">Population: </span>
-                {country?.population}
-              </p>
-              <p>
-                <span className="font-bold">Region: </span>
-                {country?.region}
-              </p>
-              <p>
-                <span className="font-bold">Sub Region: </span>
-                {country?.subregion}
-              </p>
-              <p>
-                <span className="font-bold">Capital: </span>
-                {country?.capital}
-              </p>
+              {currency && (
+                <p>
+                  <span className="font-semibold">Currencies: </span>
+                  {currency.name} - {currency.symbol}
+                </p>
+              )}
+              {country?.languages && (
+                <p>
+                  <span className="font-semibold">Languages: </span>
+                  {Object.values(country?.languages).join(", ")}
+                </p>
+              )}
             </div>
           </div>
+          <CountryBorder country={country} />
         </div>
       </div>
     </main>
