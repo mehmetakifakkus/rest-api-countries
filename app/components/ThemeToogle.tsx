a77f5e12f55f6dcd12b21211f5c21f95ba4176ec
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-04 23:40:59 +0300

    Remove redundant import

--- app/components/ThemeToogle.tsx
+++ app/components/ThemeToogle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useContext } from "react";
+import React from "react";
 import { useThemeContext } from "../context/ThemeContext";
 
 export default function ThemeToogle() {
