352b42ee98341905d6f6b79a7b12dd3168d77b81
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-04 23:31:26 +0300

    Add navigation to homepage

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,13 +2,20 @@
 
 import React from "react";
 import ThemeToogle from "./ThemeToogle";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const router = useRouter();
   return (
-    <div className="h-20 bg-slate-300 dark:bg-slate-700 fixed w-full">
+    <div className="h-20 bg-slate-300 dark:bg-slate-700 fixed w-full z-20">
       <div className="flex justify-between items-center h-full px-8">
         <div className="flex items-center">
-          <div className="text-2xl font-semibold text-slate-700 dark:text-slate-50">
+          <div
+            className="text-lg sm:text-2xl font-semibold text-slate-700 dark:text-slate-50 hover:cursor-pointer"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
             Where in the world?
           </div>
         </div>
